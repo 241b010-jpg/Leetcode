b3ff011d3cbb397210104378f2f59adb77d58313
241b010-jpg <241b010@juetguna.in>
2026-04-17 23:28:24 +0530

    Time: 5 ms (29.34%) | Memory: 10.8 MB (86.76%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> stack;
+        string curr;
+        
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (curr == "" || curr == ".") {
+                
+                } else if (curr == "..") {
+                    if (!stack.empty()) {
+                        stack.pop_back();
+                    }
+                } else {
+                    stack.push_back(curr);
+                }
+                curr = "";
+            } else {
+                curr += path[i];
+            }
+        }
+        
+        string result = "/";
+        for (int i = 0; i < stack.size(); i++) {
+            result += stack[i];
+            if (i != stack.size() - 1) {
+                result += "/";
+            }
+        }
+        
+        return result;
+    }
+};
