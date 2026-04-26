db14ae1447084f49d7629a22cbaf98028d80affe
241b010-jpg <241b010@juetguna.in>
2026-04-27 01:52:25 +0530

    Time: 24 ms (72.71%) | Memory: 63 MB (87.60%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int m, n;
+    vector<vector<bool>> visited;
+    vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    bool dfs(vector<vector<char>>& grid, int x, int y, int px, int py) {
+        visited[x][y] = true;
+        
+        for (auto& d : dirs) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+            
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n)
+                continue;
+            
+            if (grid[nx][ny] != grid[x][y])
+                continue;
+ 
+            if (!visited[nx][ny]) {
+                if (dfs(grid, nx, ny, x, y))
+                    return true;
+            }
+
+            else if (!(nx == px && ny == py)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    bool containsCycle(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        visited.assign(m, vector<bool>(n, false));
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    if (dfs(grid, i, j, -1, -1))
+                        return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
